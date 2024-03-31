8becc4555975931eb37b9efc8659fff868f977d4
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-03-31 23:06:39 +0200

    [1] [Time Beats: 72.55%] [Memory Beats: 97.83%] - LeetPush

--- 1-Two-Sum.java
+++ 1-Two-Sum.java
@@ -3,17 +3,13 @@ class Solution {
 
         Map <Integer, Integer> numMap = new HashMap<>();
 
-        //Build the hash table
-        for(int i=0; i < nums.length; i++){
-            numMap.put(nums[i], i);
-        }
-
         for(int i=0; i<nums.length; i++){
             int complement = target - nums[i];
-            //if the complement exist as a key and it's value (index) not equal i
-            if(numMap.containsKey(complement) && numMap.get(complement) != i){
+            if(numMap.containsKey(complement)){
                 return new int[]{i, numMap.get(complement)};
             }
+
+            numMap.put(nums[i], i);
         }
         return new int[0];
     }
