33639474df6fc4e8dd9c38278e86ca608c9fe99c
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-03-31 22:01:21 +0200

    [242] [Time Beats: 5.42%] [Memory Beats: 89.44%] - LeetPush

--- 242-Valid-Anagram.java
+++ 242-Valid-Anagram.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-     
-    Map<Character, Integer> map = new HashMap<>();
+        if (s.length() != t.length())
+            return false;
+            
+        char[] word1 = s.toCharArray();
+        char[] word2 = t.toCharArray();
+        int n = word1.length;
 
-     for(char x : s.toCharArray()){
-        map.put(x, map.getOrDefault(x, 0) + 1);
-     }
+        Arrays.sort(word1);
+        Arrays.sort(word2);
 
-     for(char x : t.toCharArray()){
-        map.put(x, map.getOrDefault(x, 0) - 1);
-     }
-
-     for(int val : map.values()){
-        if(val !=0) return false;
-     }
-     return true;
-}
+        for(int i=0; i< n; i++){
+                if(word1[i] != word2[i])
+                return false;
+        }
+        return true;
+        
+    }
 }
