674ac3e07adaf8fb012fb6c72067066790b26f76
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-04-05 07:40:07 +0200

    [49] [Time Beats: 14.63%] [Memory Beats: 22.44%] - LeetPush

--- 49-Group-Anagrams.java
+++ 49-Group-Anagrams.java
@@ -1,47 +1,27 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-     //Method 1: Sorting by frequency:
-    //  1) build function to get the frequency
-    //  2) create HashMap that store string and list of strings
-    //  3) Populate the HashMap and return the result
+     //Method 1: Catogrize by Sorting:
 
-    //creat the HashMap 
-    if(strs == null || strs.length == 0) return new ArrayList<>();
-    Map<String , List<String>> frequencyMap = new HashMap<>();
+        //1) Sorting strings in the array
+        //2) populate the HashMap to store the sorted string and List of Strings that Angrams
 
-    for(String str: strs){
-        String frequencyString = getFrequency(str);
+        if(strs == null || strs.length == 0) return new ArrayList<>();
 
-        if(frequencyMap.containsKey(frequencyString)){
-            frequencyMap.get(frequencyString).add(str);
-        }else{
-            List<String> strList = new ArrayList<>();
-             strList.add(str);
-            frequencyMap.put(frequencyString, strList);
+        Map<String, List<String>> angramsMap =  new HashMap<>();
+
+        for(String s : strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = String.valueOf(arr);
+            if(!angramsMap.containsKey(key)){
+                angramsMap.put(key, new ArrayList<>());
+            }
+            angramsMap.get(key).add(s);
+
+        } 
+        List<List<String>> resultList = new ArrayList<>();
+        for(Map.Entry<String, List<String>> stringAngrams: angramsMap.entrySet()){
+            resultList.add(stringAngrams.getValue());
         }
-    }
-
-    return new ArrayList<>(frequencyMap.values());
-    }
-    // build function to get the frequency
-    private String getFrequency(String str){
-
-        int[] freq = new int[26];
-
-        for(char c : str.toCharArray()){
-            freq[c - 'a']++;
-        }
-
-        //create string after calculate the frequency \e1a1t1\
-        StringBuilder frequencyString = new StringBuilder(\\);
-        char c ='a';
-        for(int i : freq){
-            frequencyString.append(c);
-            frequencyString.append(i);
-            c++;
-        }
-        return frequencyString.toString();
-    }
-}
-    
-    
+        return resultList;
+    }}
