adf41143a839f4119d5ad21b7c60ee72087a7435
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-04-05 07:51:34 +0200

    [49] [Time Beats: 70.44%] [Memory Beats: 65.72%] - LeetPush

--- 49-Group-Anagrams.java
+++ 49-Group-Anagrams.java
@@ -5,22 +5,24 @@ class Solution {
         //1) Sorting strings in the array
         //2) populate the HashMap to store the sorted string and List of Strings that Angrams
 
-        if(strs == null || strs.length == 0) return new ArrayList<>();
+        if(strs == null || strs.length ==0) return new ArrayList<>();
 
-        Map<String, List<String>> angramsMap =  new HashMap<>();
+        Map<String, List<String>> angramsMap = new HashMap<>();
 
+//Populate the HashMap with sorted strings and strings 
         for(String s : strs){
             char[] arr = s.toCharArray();
             Arrays.sort(arr);
-            String key = String.valueOf(arr);
+            String key = String.valueOf(arr); //Convert CharArray to String
+
             if(!angramsMap.containsKey(key)){
                 angramsMap.put(key, new ArrayList<>());
             }
             angramsMap.get(key).add(s);
-
-        } 
+        }
+        //Iterate through the HashMap
         List<List<String>> resultList = new ArrayList<>();
-        for(Map.Entry<String, List<String>> stringAngrams: angramsMap.entrySet()){
+        for(Map.Entry<String, List<String>> stringAngrams : angramsMap.entrySet()){
             resultList.add(stringAngrams.getValue());
         }
         return resultList;
