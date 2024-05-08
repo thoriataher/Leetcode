16b0cb37403c08a907ee1e3ca31a871335216e38
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-05-08 23:19:32 +0300

    [424] [Time Beats: 27.25%] [Memory Beats: 29.86%] - LeetPush

---
+++ 424-Longest-Repeating-Character-Replacement.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int windowSize = 0;
+        int windowStart = 0;
+        int maxLength = 0;
+        int maxRepeatCharCount = 0;
+
+        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
+            char c = s.charAt(windowEnd);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            maxRepeatCharCount = Math.max(maxRepeatCharCount, map.get(c));
+
+            windowSize = windowEnd - windowStart + 1;
+            if (windowSize - maxRepeatCharCount > k) {
+                char leftChar = s.charAt(windowStart);
+                map.put(leftChar, map.get(leftChar) - 1);
+                windowStart++;
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+        return maxLength;
+    }
+}
