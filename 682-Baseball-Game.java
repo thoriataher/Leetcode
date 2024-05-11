1851089f2df899e618ac641c16b0687eaf67382f
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-05-11 04:31:05 +0300

    [682] [Time Beats: 46.69%] [Memory Beats: 15.22%] - LeetPush

---
+++ 682-Baseball-Game.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < operations.length; i++) {
+            if (operations[i].equals(\C\)) {
+                stack.pop();
+            } 
+            else if(operations[i].equals(\D\)) {
+                stack.push(2* stack.peek());
+            }
+            else if (operations[i].equals(\+\) && stack.size()>=2) {
+                int x = stack.pop();
+                int y = stack.pop();
+                int z = x+y;
+
+                stack.push(y);
+                stack.push(x);
+                stack.push(z);
+            } else {
+                stack.push(Integer.parseInt(operations[i]));
+            }
+        }
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        return sum;
+    }
+}
