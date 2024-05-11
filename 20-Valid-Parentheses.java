72a7f118ce41ac4353edc14295f30cf6dba1c1ff
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-05-11 03:28:18 +0300

    [20] [Time Beats: 55.32%] [Memory Beats: 74.83%] - LeetPush

--- 20-Valid-Parentheses.java
+++ 20-Valid-Parentheses.java
@@ -1,24 +1,19 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
+
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
         for (char ch : s.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
+            if (map.containsValue(ch)) {
                 stack.push(ch);
-            } else {
-                if (ch == ')') {
-                    if (stack.isEmpty() || stack.pop() != '(') {
-                        return false;
-                    }
-                }
-                if (ch == '}') {
-                    if (stack.isEmpty() || stack.pop() != '{') {
-                        return false;
-                    }
-                }
-                if (ch == ']') {
-                    if (stack.isEmpty() || stack.pop() != '[' ) {
-                        return false;
-                    }
+            }
+            if (map.containsKey(ch)) {
+                if (stack.isEmpty() || map.get(ch) != stack.pop()) {
+                    return false;
                 }
             }
         }
