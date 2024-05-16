30d7b718b6f5f9d444b2977b8fdf673c50814cbe
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-05-17 01:22:02 +0300

    [977] [Time Beats: 45.75%] [Memory Beats: 84.49%] - LeetPush

--- 977-Squares-of-a-Sorted-Array.java
+++ 977-Squares-of-a-Sorted-Array.java
@@ -1,10 +1,8 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int[] res = new int[nums.length];
-        int i = 0;
-        while(i < nums.length){
+        for(int i=0; i < nums.length; i++){
             res[i] = nums[i] * nums[i];
-            i++;
         }
         Arrays.sort(res);
         return res;
