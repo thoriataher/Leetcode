e2ad57086333ef1ad02fbda0114a514345de5985
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-06-01 02:37:14 +0200

    [260] [Time Beats: 31.79%] [Memory Beats: 53.31%] - LeetPush

--- 260-Single-Number-III.java
+++ 260-Single-Number-III.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         Map<Integer, Integer> freq = new HashMap<>();
+        int[] res = new int[2];
+
+        // Count the frequency of each number
         for (int num : nums) {
             freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
 
-        int[] res = new int[2];
+        // Find the distinct numbers
         int index = 0;
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
-            if (entry.getValue() == 1) {
-                res[index++] = entry.getKey();
-                if (index == 2) {
-                    break;
-                }
-            }
+        for (int num : freq.keySet()) {
+            if (freq.get(num) == 1)
+                res[index++] = num;
         }
+
         return res;
     }
 }
