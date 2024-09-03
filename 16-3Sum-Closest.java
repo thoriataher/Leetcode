9863d863921aca0721f90793a90e38dd116d61ff
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-09-03 02:29:36 +0200

    [16] [Time Beats: 47.30%] [Memory Beats: 71.79%] - LeetPush

--- 16-3Sum-Closest.java
+++ 16-3Sum-Closest.java
@@ -1,19 +1,20 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2];
+        int closestSum = nums[0] + nums[1] + nums[2]; // intial value
 
-        for (int i = 0; i < nums.length - 2; i++) { // fix the first elements
-            // 2Ptrs approach
+        for (int i = 0; i < nums.length - 2; i++) {// fix the first element
+            // using 2 ptrs to track the other 2 elements
             int left = i + 1;
             int right = nums.length - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-
+                // comparison to find the closest sum
+                // as long as the difference is small -> it's the closest
                 if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
                     closestSum = sum;
                 }
-
+                // move the ptrs.
                 if (sum < target) {
                     left++;
                 } else {
