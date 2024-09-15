59c85af54b5749e574d54ecb90de6c5fd0c61191
Thoria <113485245+thoriataher@users.noreply.github.com>
2024-09-15 05:17:00 +0300

    [2] [Time Beats: 100.00%] [Memory Beats: 71.60%] - LeetPush

--- 2-Add-Two-Numbers.java
+++ 2-Add-Two-Numbers.java
@@ -8,72 +8,28 @@
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- import java.math.BigInteger;
-
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // should I call converter and use the returned value?
-        int[] arr1 = convertListNodeToArray(l1);
-        int[] arr2 = convertListNodeToArray(l2);
-
-        BigInteger num1 = convertArrayToNumber(reverseArray(arr1));
-        System.out.println(num1);
-        BigInteger num2 = convertArrayToNumber(reverseArray(arr2));
-        System.out.println(num2);
-        BigInteger sum = num1.add(num2);
-
-        return convertNumberToListNode(sum);
-    }
-
-    // function to convert ListNode into number
-    private int[] convertListNodeToArray(ListNode list) {
-        List<Integer> tempList = new ArrayList<>();
-        while (list != null) {
-            tempList.add(list.val);
-            list = list.next;
+        ListNode result = new ListNode(0);
+        ListNode ptr = result;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = 0 + carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            sum = sum % 10;
+            ptr.next = new ListNode(sum);
+            ptr = ptr.next;
         }
-        // convert arraylist into array
-        int[] arr = new int[tempList.size()];
-        for (int i = 0; i < tempList.size(); i++) {
-            arr[i] = tempList.get(i);
-        }
-        return arr;
-    }
-
-    // convert array to number
-    private BigInteger convertArrayToNumber(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int num : arr) {
-            sb.append(num);
-        }
-        return new BigInteger(sb.toString());
-    }
-
-    // Reverse an array
-    private int[] reverseArray(int[] arr) {
-        int[] reversed = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            reversed[i] = arr[arr.length - 1 - i];
-        }
-        return reversed;
-    }
-
-    // convert number to ListNode
-    private ListNode convertNumberToListNode(BigInteger number) {
-        if (number.equals(BigInteger.ZERO)) {
-            return new ListNode(0);
-        }
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-
-        while (!number.equals(BigInteger.ZERO)) {
-            int digit = number.mod(BigInteger.TEN).intValue();
-            current.next = new ListNode(digit);
-            current = current.next;
-            number = number.divide(BigInteger.TEN);
-
-        }
-        return dummy.next;
+        if (carry == 1)
+            ptr.next = new ListNode(1);
+        return result.next;
     }
 }
